a8d229ccb739045e4900bb26529d979ec92ce979
Fangde Liu <hibana1983@gmail.com>
2018-06-19 17:36:27 +0800

    update code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 alert("the great day");
+
+var myage=25
+
+if (myage >20 && myage<30)
+{
+    console.log("you can come");
+    
+}
+else
+    {
+        console.log("no ,you can not");
+        
+    }
